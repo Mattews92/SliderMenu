781ab1f15429f25a293b84bea434afe3dee5aa70
Mathews <mathews.philip@experionglobal.com>
2017-05-17 11:44:26 +0530

    Initial Commit

---
+++ SliderMenu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SliderMenu
+//
+//  Created by Mathews on 17/05/17.
+//  Copyright © 2017 mathews. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
